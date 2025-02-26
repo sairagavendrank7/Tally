9c4628694763db4ed41ab0dc1e3e1cc366d0a17c
sairagavendrank7 <saisuperbadges@gmail.com>
2025-02-26 10:19:10 +0530

    New Commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const xml2js = require('xml2js');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Function to send XML request to Tally Web Server to fetch data
 async function fetchTallyData() {
